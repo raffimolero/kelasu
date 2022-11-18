b007c65d894c0b07bf9aa6a90980b32a33772064
raffimolero <raffimolero@gmail.com>
2022-11-18 22:13:42 +0800

    more descriptive responses, removed /age

--- kelasu-bot/src/main.rs
+++ kelasu-bot/src/main.rs
@@ -21,6 +21,7 @@ enum TeamPreference {
 
 #[derive(Debug)]
 struct Lobby {
+    id: LobbyId,
     /// the first player is the host.
     players: Vec<UserId>,
     channel: ChannelId,
@@ -28,24 +29,25 @@ struct Lobby {
 }
 
 impl Lobby {
-    fn new(host: UserId, channel: ChannelId) -> Self {
+    fn new(id: LobbyId, host: UserId, channel: ChannelId) -> Self {
         Self {
+            id,
             players: vec![host],
             channel,
             state: LobbyState::new(),
         }
     }
 
-    /// asks both players which sides they prefer, at the same time
+    /// asks both players which sides they prefer.
     async fn get_user_teams(
         ctx: Context<'_>,
-        player_ids: [UserId; 2],
+        players: [UserId; 2],
     ) -> Result<[TeamPreference; 2], serenity::Error> {
         let reply = ctx
             .send(|m| {
                 m.content(format!(
-                    "<@{}> <@{}>\nWhich sides would you like to be on?\n(No, the interaction did not fail. Your opponent is choosing their side as well.)",
-                    player_ids[0], player_ids[1]
+                    "<@{}> <@{}>\nWhich sides would you like to be on?\n||You can change sides if the interaction 'fails', but don't worry, your previous preference is recorded until the game begins.||",
+                    players[0], players[1]
                 ))
                 .components(|c| {
                     c.create_action_row(|r| {
@@ -80,7 +82,7 @@ impl Lobby {
 
             let Some(interaction) = &message
                 .await_component_interaction(ctx.discord())
-                .filter(move |interaction| player_ids.contains(&interaction.user.id))
+                .filter(move |interaction| players.contains(&interaction.user.id))
                 .await else {
                     ctx.say("You didn't interact in time. Your preference has been set to 'Either'.").await?;
                     for p in prefs.iter_mut().filter(|p| p.is_none()) {
@@ -98,7 +100,7 @@ impl Lobby {
                     TeamPreference::Either
                 }
             };
-            let this = (player_ids[1] == interaction.user.id) as usize;
+            let this = (players[1] == interaction.user.id) as usize;
             prefs[this] = Some(pref);
         }
     }
@@ -116,11 +118,19 @@ impl Lobby {
         }
 
         let game = Game::new(pair[0], pair[1]);
-        ctx.say(format!(
-            "Game starting!\nBlue: <@{}>,\nRed: <@{}>.",
-            game.blue, game.red,
-        ))
-        .await?;
+        ctx.channel_id()
+            .say(
+                &ctx.discord().http,
+                format!(
+                    "Game starting!\n\
+                    Lobby: {}\n\
+                    Blue: <@{}>,\n\
+                    Red: <@{}>.\n\
+                    Good luck, have fun!",
+                    self.id, game.blue, game.red,
+                ),
+            )
+            .await?;
         self.state = LobbyState::Ongoing(game);
         Ok(())
     }
@@ -179,14 +189,15 @@ impl Data {
 /// lists all active lobbies.
 #[poise::command(slash_command, prefix_command)]
 async fn lobbies(ctx: Context<'_>) -> Result<(), Error> {
-    let response = ctx
-        .data()
-        .lobbies
-        .lock()
-        .await
-        .iter()
-        .map(|(k, v)| format!("Name: `{}` ({})\n", k, v.state))
-        .collect::<String>();
+    let lobbies = ctx.data().lobbies.lock().await;
+    if lobbies.is_empty() {
+        ctx.say("There are no active lobbies...").await?;
+        return Ok(());
+    }
+    let mut response = "Active lobbies:".to_owned();
+    for (k, v) in lobbies.iter() {
+        response.push_str(&format!("\nName: `{}` ({})", k, v.state));
+    }
     ctx.say(response).await?;
     Ok(())
 }
@@ -199,10 +210,13 @@ async fn host(
 ) -> Result<(), Error> {
     let mut lobbies = ctx.data().lobbies.lock().await;
     let response = if lobbies.contains_key(&name) {
-        "That lobby already exists."
+        "That lobby already exists.".to_owned()
     } else {
-        lobbies.insert(name, Lobby::new(ctx.author().id, ctx.channel_id()));
-        "Created lobby."
+        lobbies.insert(
+            name.clone(),
+            Lobby::new(name.clone(), ctx.author().id, ctx.channel_id()),
+        );
+        format!("Created lobby: {name}")
     };
     ctx.say(response).await?;
     Ok(())
@@ -214,37 +228,37 @@ async fn join(
     ctx: Context<'_>,
     #[description = "The name of the lobby to join."] name: String,
 ) -> Result<(), Error> {
-    let response = 'a: {
-        let mut lobbies = ctx.data().lobbies.lock().await;
-        let Some(lobby) = lobbies.get_mut(&name) else {
-            break 'a "That lobby does not exist.";
-        };
-        let player = ctx.author().id;
-        if lobby.players.contains(&player) {
-            break 'a "You cannot join the same lobby twice.";
-        }
-        if lobby.state.is_closed() {
-            break 'a "That lobby is no longer accepting players.";
-        }
-        lobby.players.push(player);
-        lobby.start(ctx).await?;
-        "Good luck, have fun!"
+    let mut lobbies = ctx.data().lobbies.lock().await;
+    let Some(lobby) = lobbies.get_mut(&name) else {
+        ctx.say("That lobby does not exist.").await?;
+        return Ok(());
     };
-    ctx.say(response).await?;
+    let player = ctx.author().id;
+    if lobby.players.contains(&player) {
+        ctx.say("You cannot join the same lobby twice.").await?;
+        return Ok(());
+    }
+    if lobby.state.is_closed() {
+        ctx.say("That lobby is no longer accepting players.")
+            .await?;
+        return Ok(());
+    }
+    lobby.players.push(player);
+    lobby.start(ctx).await?;
     Ok(())
 }
 
-/// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
-async fn age(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
-    Ok(())
-}
+// /// Displays your or another user's account creation date
+// #[poise::command(slash_command, prefix_command)]
+// async fn age(
+//     ctx: Context<'_>,
+//     #[description = "Selected user"] user: Option<serenity::User>,
+// ) -> Result<(), Error> {
+//     let u = user.as_ref().unwrap_or_else(|| ctx.author());
+//     let response = format!("{}'s account was created at {}", u.name, u.created_at());
+//     ctx.say(response).await?;
+//     Ok(())
+// }
 
 #[poise::command(prefix_command)]
 async fn register(ctx: Context<'_>) -> Result<(), Error> {
@@ -256,7 +270,7 @@ async fn register(ctx: Context<'_>) -> Result<(), Error> {
 async fn main() {
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age(), host(), join(), lobbies(), register()],
+            commands: vec![/* age(), */ host(), join(), lobbies(), register()],
             ..Default::default()
         })
         .token(dotenv::var("BOT_TOKEN").expect("missing DISCORD_TOKEN"))
