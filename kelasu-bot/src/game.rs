6137cdd571353d5158f813aa8420587bb11dfefc
raffimolero <raffimolero@gmail.com>
2022-11-20 22:06:34 +0800

    embeds, speecheth, board, robust interactions

--- kelasu-bot/src/game.rs
+++ kelasu-bot/src/game.rs
@@ -5,7 +5,11 @@ use kelasu_game::{
 };
 use poise::{
     futures_util::StreamExt,
-    serenity_prelude::{self as serenity, ButtonStyle, CreateComponents, UserId},
+    serenity_prelude::{
+        self as serenity, ButtonStyle, ComponentInteractionCollector, CreateComponents, Message,
+        UserId,
+    },
+    CreateReply,
 };
 use tokio::time::Duration;
 
@@ -66,62 +70,85 @@ impl Game {
                 fences[y][x + 1] = 0b_101;
             }
 
-            if let Some(y) = held_digit {
-                fences[y as usize][00] = 0b110;
-                fences[y as usize][10] = 0b111;
-            }
+            // if let Some(y) = held_digit {
+            //     for x in 0..11 {
+            //         if fences[y as usize][x] == 0 {
+            //             fences[y as usize][x] = 0b110;
+            //         }
+            //     }
+            // }
 
             fences
         };
 
         fn fence_icon(fence: u8) -> char {
-            b" []|()<>"[fence as usize] as char
+            b" []|()-"[fence as usize] as char
         }
 
         // this string is only for reference, it will not actually be displayed :P
-        let board_repr_len = "Power: 8\n\
+        // ║═╔╗╚
+        let board_repr_len = "\
             ```\n\
-               0 1 2 3 4 5 6 7 8 9 \n\
-            0 [ | ( )           [ ]\n\
-            1                      \n\
-            2         [ | | ]      \n\
-            3 <       ( )         >\n\
-            4          : :         \n\
-            5          : :         \n\
-            6                      \n\
-            7                      \n\
-            8                      \n\
-            9                      \n\
+            Energy: ########\n\
+               ╔─0─1─2─3─4─5─6─7─8─9─╗\n\
+             0 ║[B|B(B)B B B B B B[B]║ 0\n\
+             1 ║ B B B B B B B B B B ║ 1\n\
+             2 ║ s   s  [ | | ]s   s ║ 2\n\
+            <3=║ . . . . . . . . . . ║=3>\n\
+             4 ║         : :         ║ 4\n\
+             5 ║         : :         ║ 5\n\
+             6 ║                     ║ 6\n\
+             7 ║ s   s         s   s ║ 7\n\
+             8 ║ b b b b b b b b b b ║ 8\n\
+             9 ║ b b b b b b b b b b ║ 9\n\
+               ╚─0─1─2─3─4─5─6─7─8─9─╝\n\
             ```"
         .len();
 
-        let mut board_repr = String::with_capacity(board_repr_len);
-        board_repr.push_str("Power: ");
-        board_repr.push(char::from_digit(power.into(), 10).expect("IT'S OVER 9"));
-        board_repr.push_str("\n```\n   0 1 2 3 4 5 6 7 8 9 \n");
+        let mut out = String::with_capacity(board_repr_len);
+        out.push_str("Energy: ");
+        for _ in 0..power {
+            out.push('#');
+        }
+        out.push_str("\n```\n   ╔─0─1─2─3─4─5─6─7─8─9─╗\n");
 
         for (y, row) in self.game.board.tiles.chunks(10).enumerate() {
-            board_repr.push(char::from_digit(y as u32, 10).unwrap());
-            board_repr.push(' ');
-            for (x, tile) in row.iter().enumerate() {
-                board_repr.push(fence_icon(fences[y][x]));
-                if tile.0.is_none() && [[4, 4], [4, 5], [5, 4], [5, 5]].contains(&[y, x]) {
-                    board_repr.push(':');
-                } else {
-                    board_repr.push(tile.icon());
-                }
-            }
-            board_repr.push(fence_icon(fences[y][10]));
-            board_repr.push('\n');
-        }
-        board_repr.push_str("```");
+            let row_selected = held_digit.map_or(false, |d| d == y as i8);
+            let line = if row_selected { b"<=.=>" } else { b"     " }.map(|b| b as char);
+            let digit = char::from_digit(y as u32, 10).unwrap();
 
-        board_repr
+            out.push(line[0]);
+            out.push(digit);
+            out.push(line[1]);
+            out.push('║');
+            for (x, tile) in row.iter().enumerate() {
+                out.push(fence_icon(fences[y][x]));
+                out.push(if tile.0.is_none() {
+                    if [[4, 4], [4, 5], [5, 4], [5, 5]].contains(&[y, x]) {
+                        ':'
+                    } else {
+                        line[2]
+                    }
+                } else {
+                    tile.icon()
+                })
+            }
+            out.push(fence_icon(fences[y][10]));
+            out.push('║');
+            out.push(line[3]);
+            out.push(digit);
+            out.push(line[4]);
+            out.push('\n');
+        }
+        out.push_str("   ╚─0─1─2─3─4─5─6─7─8─9─╝\n```");
+
+        out
     }
 
     async fn select_merge(
         &self,
         ctx: Context<'_>,
+        message: &mut Message,
         player: UserId,
         piece_count: usize,
     ) -> Result<Option<PieceKind>, serenity::Error> {
@@ -133,9 +160,10 @@ impl Game {
             (PieceKind::General, '⭐'),
             (PieceKind::Stone, '💎'),
         ];
-        let reply = ctx
-            .send(|b| {
-                b.content("What piece would you like to merge?")
+
+        message
+            .edit(&ctx.discord().http, |b| {
+                b.embed(|e| e.title("What shall be born of this ritual?"))
                     .components(|c| {
                         for row in pieces.chunks(3) {
                             c.create_action_row(|r| {
@@ -171,22 +199,24 @@ impl Game {
 
         let mut interaction;
         let button = loop {
-            interaction = reply
-                .message()
-                .await?
-                .await_component_interaction(ctx.discord())
-                .timeout(Duration::from_secs(60 * 5))
-                .await;
+            interaction = message.await_component_interaction(ctx.discord()).await;
             break match &interaction {
                 Some(interaction) if interaction.user.id != player => {
                     respond_ephemeral(ctx, interaction, "It's not your turn.").await?;
                     continue;
                 }
-                Some(interaction) => interaction.data.custom_id.as_str(),
+                Some(interaction) => {
+                    interaction.defer(&ctx.discord().http).await?;
+                    interaction.data.custom_id.as_str()
+                }
                 None => "cancel",
             };
         };
-        reply.delete(ctx).await?;
+
+        message
+            .edit(&ctx.discord().http, |m| m.set_embeds(vec![]))
+            .await?;
+
         Ok(Some(match button {
             "cancel" => return Ok(None),
             "Warrior" => PieceKind::Warrior,
@@ -202,6 +232,7 @@ impl Game {
     async fn confirm_resign(
         &self,
         ctx: Context<'_>,
+        message: &mut Message,
         player: UserId,
         opponent: UserId,
     ) -> Result<bool, serenity::Error> {
@@ -224,14 +255,12 @@ impl Game {
         }
 
         let mut disabled = true;
-        let reply = ctx
-            .send(|b| {
-                b.content("Are you sure you want to resign?")
+        message
+            .edit(&ctx.discord().http, |b| {
+                b.embed(|e| e.title("Are you willing to bow down to your opponent?"))
                     .components(|c| ui(c, disabled))
             })
             .await?;
-        let mut message = reply.message().await?;
-        let message = message.to_mut();
 
         let mut timeout = Duration::from_secs(3);
         let mut interaction;
@@ -247,6 +276,7 @@ impl Game {
                     continue;
                 }
                 Some(interaction) if interaction.user.id == player => {
+                    interaction.defer(&ctx.discord().http).await?;
                     interaction.data.custom_id.as_str()
                 }
                 Some(interaction) => {
@@ -259,10 +289,7 @@ impl Game {
                     timeout = Duration::from_secs(60 * 3);
 
                     message
-                        .edit(ctx.discord(), |m| {
-                            m.content("Are you still sure you want to resign?")
-                                .components(|c| ui(c, disabled))
-                        })
+                        .edit(ctx.discord(), |m| m.components(|c| ui(c, disabled)))
                         .await?;
                     continue;
                 }
@@ -270,11 +297,15 @@ impl Game {
             };
         };
 
-        reply.delete(ctx).await?;
         let resigned = button == "resign";
         if resigned {
-            ctx.say(format!("<@{player}> resigned!")).await?;
+            ctx.say(format!("Very well... <@{player}> has resigned!"))
+                .await?;
         }
+
+        message
+            .edit(&ctx.discord().http, |m| m.set_embeds(vec![]))
+            .await?;
         Ok(resigned)
     }
 
@@ -305,12 +336,11 @@ impl Game {
                     })
             })
             .await?;
+        let mut message = reply.message().await?.into_owned();
 
         let mut interaction;
         let (response, p_move) = loop {
-            interaction = reply
-                .message()
-                .await?
+            interaction = message
                 .await_component_interaction(ctx.discord())
                 .timeout(Duration::from_secs(60 * 5))
                 .await;
@@ -341,8 +371,11 @@ impl Game {
                 None => ("Timed out. Cancelling.", Move::DeclineDraw),
             };
         };
-        reply.delete(ctx).await?;
-        ctx.say(response).await?;
+        message
+            .edit(&ctx.discord().http, |m| {
+                m.content(response).components(|c| c)
+            })
+            .await?;
 
         Ok(self.game.verify_move(p_move).unwrap())
     }
@@ -355,7 +388,7 @@ impl Game {
         prev_turn: Team,
     ) -> Result<VerifiedMove, serenity::Error> {
         ctx.say(format!(
-            "It's {} <@{player}>'s turn. You have 5 minutes to move.",
+            "It is {} <@{player}>'s turn. You have 5 minutes to lead.",
             if prev_turn == self.game.turn {
                 "still"
             } else {
@@ -366,70 +399,68 @@ impl Game {
 
         let mut held_digit = None;
         let mut positions: Vec<Pos> = Vec::with_capacity(10);
+
+        fn add_components(c: &mut CreateComponents) -> &mut CreateComponents {
+            c.create_action_row(|r| {
+                (0..5).into_iter().fold(r, |r, i| {
+                    r.create_button(|b| b.custom_id(i).label(i).style(ButtonStyle::Secondary))
+                })
+            })
+            .create_action_row(|r| {
+                (5..10).fold(r, |r, i| {
+                    r.create_button(|b| b.custom_id(i).label(i).style(ButtonStyle::Secondary))
+                })
+            })
+            .create_action_row(|r| {
+                r.create_button(|b| {
+                    b.custom_id("resign")
+                        .label("Resign")
+                        .emoji('⚠')
+                        .style(ButtonStyle::Danger)
+                })
+                .create_button(|b| {
+                    b.custom_id("draw")
+                        .label("Draw")
+                        .emoji('🤝')
+                        .style(ButtonStyle::Primary)
+                })
+                .create_button(|b| {
+                    b.custom_id("reset")
+                        .label("Reset")
+                        .emoji('🔄')
+                        .style(ButtonStyle::Primary)
+                })
+                .create_button(|b| {
+                    b.custom_id("move")
+                        .label("Move")
+                        .emoji('♐')
+                        .style(ButtonStyle::Success)
+                })
+                .create_button(|b| {
+                    b.custom_id("merge")
+                        .label("Merge")
+                        .emoji('♻')
+                        .style(ButtonStyle::Success)
+                })
+            })
+        }
+
         let reply = ctx
             .send(|b| {
                 b.content(self.board_repr(self.game.power, &positions, held_digit))
-                    .components(|c| {
-                        c.create_action_row(|r| {
-                            (0..5).into_iter().fold(r, |r, i| {
-                                r.create_button(|b| {
-                                    b.custom_id(i).label(i).style(ButtonStyle::Secondary)
-                                })
-                            })
-                        })
-                        .create_action_row(|r| {
-                            (5..10).fold(r, |r, i| {
-                                r.create_button(|b| {
-                                    b.custom_id(i).label(i).style(ButtonStyle::Secondary)
-                                })
-                            })
-                        })
-                        .create_action_row(|r| {
-                            r.create_button(|b| {
-                                b.custom_id("resign")
-                                    .label("Resign")
-                                    .emoji('⚠')
-                                    .style(ButtonStyle::Danger)
-                            })
-                            .create_button(|b| {
-                                b.custom_id("draw")
-                                    .label("Draw")
-                                    .emoji('🤝')
-                                    .style(ButtonStyle::Primary)
-                            })
-                            .create_button(|b| {
-                                b.custom_id("reset")
-                                    .label("Reset")
-                                    .emoji('🔄')
-                                    .style(ButtonStyle::Primary)
-                            })
-                            .create_button(|b| {
-                                b.custom_id("move")
-                                    .label("Move")
-                                    .emoji('♐')
-                                    .style(ButtonStyle::Success)
-                            })
-                            .create_button(|b| {
-                                b.custom_id("merge")
-                                    .label("Merge")
-                                    .emoji('♻')
-                                    .style(ButtonStyle::Success)
-                            })
-                        })
-                    })
+                    .components(|c| add_components(c))
             })
             .await?;
 
         let mut message = reply.message().await?.into_owned();
-
         let mut interactions = message
             .await_component_interactions(ctx.discord())
             .timeout(Duration::from_secs(60 * 5))
             .build();
 
+        let mut interaction;
         let p_move = loop {
-            let (interaction, rest) = interactions.into_future().await;
-            interactions = rest;
+            (interaction, interactions) = interactions.into_future().await;
 
             let button = match &interaction {
                 Some(interaction) if interaction.user.id == player => {
@@ -446,7 +477,7 @@ impl Game {
                     continue;
                 }
                 None => {
-                    ctx.say("Game Over! You didn't interact in time.").await?;
+                    ctx.say("The battle is lost! For you have left your army alone in the darkness for far too long!").await?;
                     "timeout"
                 }
             };
@@ -467,7 +498,14 @@ impl Game {
             let instruction = match button {
                 "timeout" => MakeMove(Move::Resign),
                 "resign" => {
-                    if self.confirm_resign(ctx, player, opponent).await? {
+                    let response = self
+                        .confirm_resign(ctx, &mut message, player, opponent)
+                        .await?;
+                    interactions = message
+                        .await_component_interactions(ctx.discord())
+                        .timeout(Duration::from_secs(60 * 5))
+                        .build();
+                    if response {
                         MakeMove(Move::Resign)
                     } else {
                         Noop
@@ -482,7 +520,14 @@ impl Game {
                     _ => Say("You selected too many pieces."),
                 },
                 "merge" => {
-                    if let Some(kind) = self.select_merge(ctx, player, positions.len()).await? {
+                    let response = self
+                        .select_merge(ctx, &mut message, player, positions.len())
+                        .await?;
+                    interactions = message
+                        .await_component_interactions(ctx.discord())
+                        .timeout(Duration::from_secs(60 * 5))
+                        .build();
+                    if let Some(kind) = response {
                         MakeMove(Move::Merge {
                             kind,
                             pieces: positions.clone(),
@@ -544,11 +589,14 @@ impl Game {
             message
                 .edit(&ctx.discord().http, |m| {
                     m.content(self.board_repr(self.game.power, &positions, held_digit))
+                        .components(|c| add_components(c))
                 })
                 .await?;
         };
 
-        reply.edit(ctx, |m| m.components(|c| c)).await?;
+        message
+            .edit(&ctx.discord().http, |m| m.components(|c| c))
+            .await?;
         Ok(p_move)
     }
 
@@ -557,11 +605,11 @@ impl Game {
             .say(
                 &ctx.discord().http,
                 format!(
-                    "**Game starting!**\n\
+                    "**The Battle Begins!**\n\
                     Lobby: {}\n\
                     Blue: <@{}>,\n\
                     Red: <@{}>.\n\
-                    Good luck, have fun!",
+                    May the Great win, and may the Less learn.",
                     self.lobby, self.blue, self.red,
                 ),
             )
