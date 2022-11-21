b365430f1f7ad91f48b5aa15047cafc2719e65ac
Redstoneboi <raffimolero@gmail.com>
2022-11-21 15:31:45 +0800

    i said respond_ephemeral doesn't instantly break anymore

--- kelasu-bot/src/util.rs
+++ kelasu-bot/src/util.rs
@@ -7,26 +7,21 @@ pub async fn respond_ephemeral(
     interaction: &MessageComponentInteraction,
     message: impl ToString,
 ) -> Result<(), serenity::Error> {
-    // match interaction
-    //     .get_interaction_response(&ctx.discord().http)
-    //     .await
-    // {
-    //     Ok(_response) => {
-    //         dbg!("following up...");
-    //         interaction
-    //             .create_followup_message(&ctx.discord().http, |f| {
-    //                 f.ephemeral(true).content(message)
-    //             })
-    //             .await?;
-    //     }
-    //     Err(_) => {
+    let msg = message.to_string();
     dbg!("responding...");
-    interaction
+    let Err(e) = interaction
         .create_interaction_response(&ctx.discord().http, |r| {
-            r.interaction_response_data(|d| d.ephemeral(true).content(message))
+            r.interaction_response_data(|d| d.ephemeral(true).content(&msg))
         })
+        .await
+    else {
+        return Ok(())
+    };
+
+    dbg!(e, "following up...");
+    interaction
+        .create_followup_message(&ctx.discord().http, |f| f.ephemeral(true).content(&msg))
         .await?;
-    //     }
-    // }
+
     Ok(())
 }
